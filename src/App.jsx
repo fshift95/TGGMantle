3543a87b8ea07db07ec73db8d4c4d809e727c4ae
fshift95 <fshift95@gmail.com>
2024-12-12 16:30:25 +0200

    Front-Start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline cursor-pointer">
+      {" "}
+      Welcome To Telegram And Mantle Game
+    </div>
+  );
+}
+
+export default App;
